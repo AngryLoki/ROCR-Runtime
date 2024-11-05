ca5f2b75f1affac2d7b2592af8b7afaaf3792fec
David Yat Sin <David.YatSin@amd.com>
2024-11-05 10:14:20 +0000

    rocr: Add HSA_SIGNAL_WAIT_ABORT_TIMEOUT
    Add support for abort timeout when hsa_signal_wait_relaxed is called and
    signal does not clear within timeout.
    timeout is in seconds
    
    Change-Id: If1db5a8af33c82ddc4b48968c3d8eceb97d0ea6d

--- runtime/hsa-runtime/core/util/flag.h
+++ runtime/hsa-runtime/core/util/flag.h
@@ -252,6 +252,10 @@ class Flag {
     dev_mem_queue_ = (var == "1") ? true : false;
     var = os::GetEnvVar("HSA_WAIT_ANY_DEBUG");
     wait_any_ = (var == "1") ? true : false;
+
+    /* hsa_signal_wait_relaxed abort timeout  */
+    var = os::GetEnvVar("HSA_SIGNAL_WAIT_ABORT_TIMEOUT");
+    signal_abort_timeout_ = var.empty() ? 0 : atoi(var.c_str());
   }
 
   void parse_masks(uint32_t maxGpu, uint32_t maxCU) {
@@ -365,6 +369,9 @@ class Flag {
   size_t pc_sampling_max_device_buffer_size() const { return pc_sampling_max_device_buffer_size_; }
 
   bool dev_mem_queue() const { return dev_mem_queue_; }
+  
+  uint32_t signal_abort_timeout() const { return signal_abort_timeout_; }
+
  private:
   bool check_flat_scratch_;
   bool enable_vm_fault_message_;
@@ -395,6 +402,7 @@ class Flag {
   bool enable_ipc_mode_legacy_;
   bool dev_mem_queue_;
   bool wait_any_;
+  uint32_t signal_abort_timeout_;
 
   SDMA_OVERRIDE enable_sdma_;
   SDMA_OVERRIDE enable_peer_sdma_;
